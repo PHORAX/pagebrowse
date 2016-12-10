d4a6e80db37b811a04ef9c53b8ac5ad17c5011e4
Felix Kopp <felix-source@phorax.com>
2016-12-10 17:29:46 +0100

    [TASK] Remove include_once XCLASS

--- pi1/class.tx_pagebrowse_pi1.php
+++ pi1/class.tx_pagebrowse_pi1.php
@@ -365,9 +365,3 @@ class tx_pagebrowse_pi1 extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 		return $result;
 	}
 }
-
-/** @noinspection PhpUndefinedVariableInspection */
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/pagebrowse/pi1/class.tx_pagebrowse_pi1.php'])	{
-	/** @noinspection PhpIncludeInspection */
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/pagebrowse/pi1/class.tx_pagebrowse_pi1.php']);
-}
